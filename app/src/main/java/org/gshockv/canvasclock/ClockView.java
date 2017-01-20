29c84e5156a7b684c613576e522c314ce15b7dd7
Vitaly Gashock <gshockv@gmail.com>
2017-01-20 16:21:24 +0200

    Initial commit

---
+++ app/src/main/java/org/gshockv/canvasclock/ClockView.java
@@ -0,0 +1,133 @@
+package org.gshockv.canvasclock;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class ClockView extends View {
+
+    private int height, width = 0;
+    private int padding = 0;
+    private int fontSize = 0;
+    private int numeralSpacing = 0;
+    private int handTruncation, hourHandTruncation = 0;
+
+    private int radius = 0;
+
+    private Paint paint;
+    private boolean isInit;
+    private int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+    private Rect rect = new Rect();
+
+    private int drawBgColor;
+    private int drawClockColor;
+
+    public ClockView(Context context) {
+        super(context);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void initClock() {
+        drawBgColor = getResources().getColor(R.color.colorPrimary);
+        drawClockColor = getResources().getColor(R.color.colorAccent);
+
+        height = getHeight();
+        width = getWidth();
+        padding = numeralSpacing + 50;
+
+        fontSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 13,
+                getResources().getDisplayMetrics());
+
+        int min = Math.min(height, width);
+        radius = min / 2 - padding;
+
+        handTruncation = min / 20;
+        hourHandTruncation = min / 7;
+
+        paint = new Paint();
+        isInit = true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!isInit) {
+            initClock();
+        }
+
+        canvas.drawColor(drawBgColor);
+
+        drawCircle(canvas);
+        drawCenter(canvas);
+        drawNumeral(canvas);
+        drawHands(canvas);
+
+        postInvalidateDelayed(500);
+        invalidate();
+    }
+
+    private void drawHands(Canvas canvas) {
+        Calendar c = Calendar.getInstance();
+        float hour = c.get(Calendar.HOUR_OF_DAY);
+        hour = hour > 12 ? hour - 12 : hour;
+        drawHand(canvas, (hour  + c.get(Calendar.MINUTE) / 60) * 5f, true);
+        drawHand(canvas, c.get(Calendar.MINUTE), false);
+        drawHand(canvas, c.get(Calendar.SECOND), false);
+    }
+
+    private void drawHand(Canvas canvas, double loc, boolean isHour) {
+        double angle = Math.PI * loc / 30 - Math.PI / 2;
+        int handRadius = isHour ? radius - handTruncation - hourHandTruncation : radius - handTruncation;
+        canvas.drawLine(width / 2, height / 2,
+                (float) (width / 2 + Math.cos(angle) * handRadius),
+                (float) (height / 2 + Math.sin(angle) * handRadius),
+                paint);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        paint.reset();
+        paint.setColor(drawClockColor);
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        canvas.drawCircle(width / 2, height / 2, radius + padding - 10, paint);
+    }
+
+    private void drawCenter(Canvas canvas) {
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(width / 2, height / 2, 12, paint);
+    }
+
+    private void drawNumeral(Canvas canvas) {
+        paint.setTextSize(fontSize);
+
+        for (int number : numbers) {
+            String n = String.valueOf(number);
+            paint.getTextBounds(n, 0, n.length(), rect);
+
+            double angle = Math.PI / 6 * (number - 3);
+            int x = (int) (width / 2 + Math.cos(angle) * radius - rect.width() / 2);
+            int y = (int) (height / 2 + Math.sin(angle) * radius + rect.height() / 2);
+
+            canvas.drawText(n, x, y, paint);
+
+
+        }
+    }
+
+}
